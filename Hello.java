8faee756e383b836a3083bc157435e30d8c85fb5
Aryan <106900422+Aryan-1121@users.noreply.github.com>
2022-09-14 09:22:29 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
   public static void main(String args[]){
-    System.out.println("Hello there !------!");
+    System.out.println("Hello there !!!");
     System.out.println("this is 2nd line !!");
 
     
